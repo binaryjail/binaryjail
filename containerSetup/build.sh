63dbedbabed598433f4215a4c2523067fe691a99
Andrey <andrey@jisto.com>
2015-01-22 17:07:59 -0500

    Updated build files to install latest Docker and Node.js

--- containerSetup/build.sh
+++ containerSetup/build.sh
@@ -6,10 +6,24 @@ if [ -z "$DOCKER" ]
 then
 	echo "Docker not found... Press Enter to install (requires admin privileges), ctrl-c to quit:"
 	read line
-	sudo apt-get update && sudo apt-get install -y docker.io
+	#sudo apt-get update && sudo apt-get install -y docker.io
+	sudo apt-get update && sudo apt-get install -y curl
+	curl -sSL https://get.docker.com/ubuntu/ | sudo sh
 	sudo chmod a+rw /var/run/docker.sock
 else
 	echo "Found docker at $DOCKER..."
+fi
+
+#Check Node.js install
+NODE=$(which node)
+if [ -z "$NODE" ]
+then
+	echo "Node.js not found... Press Enter to install (requires admin privileges), ctrl-c to quit:"
+	read line
+	sudo apt-get update && sudo apt-get install -y nodejs
+	sudo ln -s /usr/bin/nodejs /usr/local/bin/node
+else
+	echo "Found node.js at $NODE..."
 fi
 
 #Check ssh key
